dd5955c355b1d7b0044b629ecd78e60e99959776
param <976944083@qq.com>
2022-02-22 18:02:35 +0800

    repair the bug of register and login

--- src/main/java/com/param/bootbooks/controller/LoginController.java
+++ src/main/java/com/param/bootbooks/controller/LoginController.java
@@ -1,10 +1,9 @@
 package com.param.bootbooks.controller;
 
-import com.alibaba.druid.util.StringUtils;
 import com.param.bootbooks.pojo.User;
 import com.param.bootbooks.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +14,7 @@ import javax.servlet.http.HttpSession;
 /**
  * @author zhoujingyu（976944083@qq.com）
  */
+@Slf4j
 @Controller
 @RequestMapping("/user")
 public class LoginController {
@@ -55,8 +55,9 @@ public class LoginController {
     }
 
     @RequestMapping("/logout")
-    public String goOut(HttpSession session, Model model) {
-        session.removeAttribute("userLoginInfo");
+    public String logOut(HttpSession session, Model model) {
+        System.out.println("====================================");
+        session.removeAttribute("loginUser");
         model.addAttribute("errorMsg", "请重新登录");
         return "login";
     }
@@ -76,7 +77,6 @@ public class LoginController {
         }
 
         userService.addUser(user);
-        model.addAttribute("errorMsg",null);
         return "redirect:login";
     }
 
